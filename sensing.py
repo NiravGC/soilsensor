465796b2ba186c8e3b69c22621e8f009cce571b6
Nirav <32882197+NiravGC@users.noreply.github.com>
2019-12-01 19:22:52 +0000

    Test using BME680

--- sensing.py
+++ sensing.py
@@ -22,8 +22,11 @@ SIG -> Pin 18 (GPIO 24)
 
 # Imports
 import time
+import csv
 import bme680
 import RPi.GPIO as GPIO
+from github import Github, InputGitTreeElement
+from datetime import datetime
 
 # Temperature, Humidity, Pressure Sensor Setup
 sensor = bme680.BME680()
@@ -33,8 +36,49 @@ sensor.set_temperature_oversample(bme680.OS_8X)
 sensor.set_filter(bme680.FILTER_SIZE_3)
 
 # Moisture Sensor Setup
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(18, GPIO.IN)
+''' tbc '''
 
 # Test Readings
-output = "{0:.2f} C,{1:.2f} hPa,{2:.2f} %RH".format(sensor.data.temperature, sensor.data.pressure, sensor.data.humidity)
+def logdata():
+  now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+  temp = "{0:.1f} C".format(sensor.data.temperature)
+  press = "{0:.1f} hPa".format(sensor.data.pressure)
+  hum = "{0:.1f} %RH".format(sensor.data.humidity)
+  with open('data/sensordata.csv', 'a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow([now, temp, press, hum])
+  print("New data logged")
+  pushdata()
+
+
+# GitHub push function from stackoverflow.com/questions/38594717
+def pushdata(token='e33eeb41a8a264e5c2e737db2383a37b494a32af'):
+
+  g = Github(token)
+  repo = g.get_user().get_repo('soilsensor')
+
+  file_list = [
+    'data/sensordata.csv',
+    'data/notes.txt'
+  ]
+  commit_message = 'AUTO: New data logged'
+  master_ref = repo.get_git_ref('heads/master')
+  master_sha = master_ref.object.sha
+  base_tree = repo.get_git_tree(master_sha)
+  element_list = list()
+  for entry in file_list:
+      with open(entry, 'rb') as input_file:
+          data = input_file.read()
+      element = InputGitTreeElement(entry, '100644', 'blob', data)
+      element_list.append(element)
+  tree = repo.create_git_tree(element_list, base_tree)
+  parent = repo.get_git_commit(master_sha)
+  commit = repo.create_git_commit(commit_message, tree, [parent])
+  master_ref.edit(commit.sha)
+  print("Updated data pushed to GitHub")
+
+while True:
+  time.sleep(10)
+  logdata()
+  
+  
