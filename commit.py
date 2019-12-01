8f6005086ba6c0a666146d9c0311cd5aa2a9295c
Nirav <32882197+NiravGC@users.noreply.github.com>
2019-12-01 18:42:00 +0000

    Update commit.py

--- commit.py
+++ commit.py
@@ -6,7 +6,7 @@ token = 'e33eeb41a8a264e5c2e737db2383a37b494a32af'
 g = Github(token)
 
 repo = g.get_user().get_repo('soilsensor')
-file = 'soilsensor/data/sensordata.csv'
+file = 'data/sensordata.csv'
 
 commit_message = 'New data'
 master_ref = repo.get_git_ref('heads/master')
